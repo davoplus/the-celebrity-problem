fe84e4f7a65c6ac4ab5675b753de3859485b6987
David Alejandro Caicedo de la Cruz <david.caicedo@globant.com>
2018-07-24 21:19:54 -0500

    master add java doc

--- src/main/java/globantvividseats/thecelebrityproblem/RelationExistsException.java
+++ src/main/java/globantvividseats/thecelebrityproblem/RelationExistsException.java
@@ -3,6 +3,8 @@ package globantvividseats.thecelebrityproblem;
 public class RelationExistsException extends Exception {
 
 	/**
+	 * This exception cover the "The relation already exists" case
+	 * @author david.caicedo
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
